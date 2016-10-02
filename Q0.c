ac812ea8569b1ea325efe44328bda133db38b1c8
God-ComplexKid <wsteven928@yahoo.com>
2016-10-02 15:07:27 -0700

    Update Q0.c

--- Q0.c
+++ Q0.c
@@ -1,6 +1,6 @@
 /* CS261- Assignment 1 - Q. 0*/
-/* Name:
- * Date:
+/* Name: Steven Walter
+ * Date: 10/1/16
  * Solution description:
  */
  
@@ -8,23 +8,36 @@
 #include <stdlib.h>
 
 int fooB(int* a, int* b, int c) {
+	c = (*a)+(*b);
+	int l = (*b);
+	(*a) = l;
+	*b = (*b)+(*b);
 
+	return c;
 }
 
 
 void fooA(int* iptr){
-
+	printf("intger: %d adres: %p adresofadres: %p \n", *iptr, iptr, &iptr);
 }
 
 int main(){
     
   /* declare an integer x */
+  int x = 10;
   
   /* print the address of x */
-    
+  printf("%p \n", &x);
   /* Call fooA() with the address of x */
-    
+  fooA(&x);  
   /* print the value of x */
-    
+  printf("%d \n", x);
+  int o = 7;
+  int p = 3;
+  int k;
+  k = fooB(&o, &p, k);
+  printf("A: %d \n", o);
+  printf("B: %d \n", p);
+  printf("C : %d \n", k);
   return 0;
 }
